8fa814205737c20f9f934ec522559461eea4249d
faruk4234 <faruk.cetin4234@gmail.com>
2022-09-20 20:31:35 +0300

    group

--- app/Modules/User/routes.ts
+++ app/Modules/User/routes.ts
@@ -1,16 +1,19 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return 'HELLO WORLD'
-})
+//using group for routes
+Route.group(() => {
+  Route.get('/', async () => {
+    return 'HELLO WORLD'
+  })
 
-Route.get('/ciguli', async () => {
-  return 'cigubigule'
-})
+  Route.get('/ciguli', async () => {
+    return 'cigubigule'
+  })
 
-Route.get('/posts/:id', async () => {
-  const postUrl = Route.makeUrl('posts.show', { id: 1 })
-  return postUrl
-}).as('post.index')
+  Route.get('/posts/:id', async () => {
+    const postUrl = Route.makeUrl('posts.show', { id: 1 })
+    return postUrl
+  }).as('post.index')
+}).prefix('/user')
 
 Route.on('/goBr').redirectToPath('https://tr.tradingview.com/symbols/BTCUSDT/')
